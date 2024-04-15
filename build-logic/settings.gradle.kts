a355244e810a330c01cfa6f348ab925223d330c6
daz <daz@gradle.com>
2024-04-15 07:24:20 -0600

    Connect to ge.solutions-team.gradle.com

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -13,6 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+plugins {
+    id("com.gradle.develocity") version "3.17.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0"
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+}
+
+buildCache {
+    remote(develocity.buildCache) {
+        isPush = System.getenv("CI") != null
+    }
+}
 
 dependencyResolutionManagement {
     repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,21 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version "3.17.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0"
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+}
+
+buildCache {
+    remote(develocity.buildCache) {
+        isPush = System.getenv("CI") != null
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
